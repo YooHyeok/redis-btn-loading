b657c9b98f6e96ca9d0fc8ca282715eef3a1f4a9
YooHyeok <yjk7454@naver.com>
2025-07-19 14:56:41 +0900

    feat: init project

---
+++ src/test/java/com/redis/button/ButtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.button;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
